b257d893522e31bd6836740a1f679bf08c0ee68e
Levy Cen <levy.cen@meru.com>
2021-10-25 18:47:10 -0500

    Scaffolds, DB, and annotate

--- commands_example.rb
+++ commands_example.rb
@@ -3,3 +3,31 @@ cd organizador
 
 yarn add bootstrap jquery popper.js roboto-fontface
 git commit -m "initial commit"
+
+##########=====================================
+Video 20 - Construye los primeros scaffolds del proyecto - Platzi
+
+rails g simple_form:install --bootstrap
+
+rails g scaffold Category name:string description:text
+
+rails g scaffold Task name:string description:text due_date:date category:references
+
+configurar db config/database.yml
+
+sudo -i -u postgres
+createuser --pwprompt --interactive platzi
+
+=> Enter password for new role:  platzi
+=> Enter it again: platzi
+=> Shall the new role be a superuser? (y/n) y
+=> Shall the new role be allowed to create databases? y
+=> Shall the new role be allowed to create more new roles? (y/n) n
+exit
+
+rails db:create
+rails db:migrate
+
+adding gem 'annotate' to Gemfile
+bi
+annotate --models
